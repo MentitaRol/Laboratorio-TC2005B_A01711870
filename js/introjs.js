c37f3635fbc47671870103d650fdeb97bf2f7b81
Luisa Fernanda Valdez Guillen <A01711870@tec.mx>
2025-02-13 10:45:22 -0600

    Constantes y variables

--- js/introjs.js
+++ js/introjs.js
@@ -10,3 +10,10 @@ console.assert(1 == "1");
 console.assert(1 === "1"); //Tripe igual compara valores y tipo de dato -> 1 === "1" es falso
 console.assert(1 == true);
 console.assert(1 === true);
+
+// variables, constantes
+
+var wookie = "Cheewbaca"; //Forma antigua, no es tan segura
+let otro_wookie = "Tarful"; //Forma moderna, tiene mayor alcance
+
+const precio = 99.99;
