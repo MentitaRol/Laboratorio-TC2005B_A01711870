b686eb5fe2853b3c92e70d61fdcf95c89d5efd5e
Luisa Fernanda Valdez Guillen <A01711870@tec.mx>
2025-02-13 10:56:29 -0600

    fundamentos de js

--- js/introjs.js
+++ js/introjs.js
@@ -17,3 +17,40 @@ var wookie = "Cheewbaca"; //Forma antigua, no es tan segura
 let otro_wookie = "Tarful"; //Forma moderna, tiene mayor alcance
 
 const precio = 99.99;
+
+//Alcance de las variables
+
+// Utilizando "let" es más seguro ya que fuera del ciclo for la variable ya no existe
+for(let i = 0; i < 10; i++){
+    console.log(i);
+}
+// Ya no imprime "i"
+// console.log(i);
+
+// Utilizando "var" fuera del ciclo for la variable sigue existiendo
+for(var i = 0; i < 10; i++){
+    console.log(i);
+}
+// Imprime "i"
+console.log(i);
+
+
+//alert, prompt, confirm
+
+alert("aadkakdmsakdksamd");
+const nombre = prompt("¿Como te llamas?");
+const is_hambre = confirm("¿Tienes hambre?");
+
+// funciones tradicionales
+
+function imprimir_status(){
+    let respuesta = "";
+    if(is_hambre){
+        respuesta = "tiene hambre";
+    }
+    console.log(nombre + respuesta);
+}
+
+// funciones modernas
+
+() => {}
