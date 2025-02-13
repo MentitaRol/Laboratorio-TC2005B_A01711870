240e73c71d4763733649302a0cf1fcdd514e8484
Luisa Fernanda Valdez Guillen <A01711870@tec.mx>
2025-02-13 10:39:52 -0600

    Funciones de consola en js

---
+++ js/introjs.js
@@ -0,0 +1,12 @@
+// consola (log, info, warm, error, assert)
+
+console.log("Hola mundo!"); //Hola mundo desde consola
+console.info("Aqui andamos aprendiendo");
+console.warn("Esta es una advertencia");
+console.error("kjaksjkasjkdjskdjskads");
+console.assert(1 == 1);
+console.assert(1 === 1);
+console.assert(1 == "1");
+console.assert(1 === "1"); //Tripe igual compara valores y tipo de dato -> 1 === "1" es falso
+console.assert(1 == true);
+console.assert(1 === true);
