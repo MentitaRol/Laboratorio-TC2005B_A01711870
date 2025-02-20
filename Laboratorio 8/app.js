ba6371490fc3cd9912ee363202868efab62d1c57
Luisa Fernanda Valdez Guillen <A01711870@tec.mx>
2025-02-20 10:14:10 -0600

    asynchronous sort

--- Laboratorio 8/app.js
+++ Laboratorio 8/app.js
@@ -4,3 +4,13 @@ const filesystem = require('fs');
 
 //crea un archivo .txt y escribe en el 'Hola desde node'
 filesystem.writeFileSync('hola.txt', 'Hola desde node');
+
+
+//sort asincrono
+const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 500];
+
+for(let item of arreglo){
+    setTimeout(() => {
+        console.log(item);
+    }, item);
+}
