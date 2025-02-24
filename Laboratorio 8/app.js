f4a8a0306f88f990ad5d9b6a0000488b07e4450b
Luisa Fernanda Valdez Guillen <A01711870@tec.mx>
2025-02-24 09:46:40 -0600

    form con input y label

--- Laboratorio 8/app.js
+++ Laboratorio 8/app.js
@@ -153,6 +153,21 @@ const html = `<!DOCTYPE html>
             </article>
         </div>
 
+        <form action="/" method="POST">
+            <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+            <div class="form-floating">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="floatingInput">Email address</label>
+            </div><br>
+
+            <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <label for="floatingPassword">Password</label>
+            </div><br>
+
+            <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button><br><br>
+        </form>
     </section>
 
 </body>
