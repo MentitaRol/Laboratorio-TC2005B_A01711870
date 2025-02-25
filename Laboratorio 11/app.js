635ebf537a70156b04d470b1745c740379fbdc5a
Luisa Fernanda Valdez Guillen <A01711870@tec.mx>
2025-02-25 11:36:26 -0600

    separar archivos en modulos

--- Laboratorio 11/app.js
+++ Laboratorio 11/app.js
@@ -1,37 +1,3 @@
-const html_content = `<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lab 6: Programación orientada a eventos</title>
-    <!-- Booststrap link -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <section class="container">
-        <div class="row align-items-start">
-            <div class="col">
-                <form action="/nombres/agregar" method="POST">
-                    <h1 class="h3 mb-3 fw-normal">Escribe algo</h1>
-
-                    <div class="form-floating">
-                    <input type="text" class="form-control" id="floatingPassword" placeholder="Nombre" name="nombre">
-                    <label for="floatingPassword">Nombre</label>
-                    </div><br>
-
-                    <input type="submit" id="boton_submit" class="btn btn-primary w-100 py-2" value="enviar"><br><br>
-                </form>
-            </div>
-        </div>
-    </section>
-
-</body>
-</html>`;
-
-const nombres = [];
-
 const express = require('express');
 const app = express();
 
@@ -61,31 +27,7 @@ app.use( '/ruta', (request, response, next) => {
     response.send('Respuesta de la ruta "/ruta"');
 }); 
 
-//Cuando se registra un middleware con app.get(), 
-//el middleware sólo se registra para el método HTTP GET
-app.get('/nombres/agregar', (request, response, next) => {
-    response.send(html_content);
-});
-
-//Cuando se registra un middleware con app.post(), 
-//el middleware sólo se registra para el método HTTP POST
-app.post('/nombres/agregar', (request, response, next) => {
-    //request.body es un objeto que contiene los datos enviados en el cuerpo de la solicitud HTTP
-    console.log(request.body);
-    nombres.push(request.body.nombre);
-    console.log(nombres);
-    let html = html_content;
-    for(let nombre of nombres){
-        html += nombre;
-    }
-    response.send(html);
-});
-
-app.use((request, response, next) => {
-    console.log('Otro middleware!');
-
-    //Manda la respuesta
-    response.send('¡Hola mundo!'); 
-});
+const rutasNombres = require('./routes/nombres.routes');
+app.use('/nombres', rutasNombres);
 
 app.listen(3000);
---
+++ Laboratorio 11/routes/nombres.routes.js
@@ -0,0 +1,61 @@
+//Si los archivos comienzan a crecer, es importante reestructurarlos semánticamente en módulos para que sean más fáciles de mantener.
+
+const express = require('express');
+
+const router = express.Router();
+
+const html_content = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab 6: Programación orientada a eventos</title>
+    <!-- Booststrap link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section class="container">
+        <div class="row align-items-start">
+            <div class="col">
+                <form action="/nombres/agregar" method="POST">
+                    <h1 class="h3 mb-3 fw-normal">Escribe algo</h1>
+
+                    <div class="form-floating">
+                    <input type="text" class="form-control" id="floatingPassword" placeholder="Nombre" name="nombre">
+                    <label for="floatingPassword">Nombre</label>
+                    </div><br>
+
+                    <input type="submit" id="boton_submit" class="btn btn-primary w-100 py-2" value="enviar"><br><br>
+                </form>
+            </div>
+        </div>
+    </section>
+
+</body>
+</html>`;
+
+const nombres = [];
+
+//Cuando se registra un middleware con app.get(), 
+//el middleware sólo se registra para el método HTTP GET
+router.get('/agregar', (request, response, next) => {
+    response.send(html_content);
+});
+
+//Cuando se registra un middleware con app.post(), 
+//el middleware sólo se registra para el método HTTP POST
+router.post('/agregar', (request, response, next) => {
+    //request.body es un objeto que contiene los datos enviados en el cuerpo de la solicitud HTTP
+    console.log(request.body);
+    nombres.push(request.body.nombre);
+    console.log(nombres);
+    let html = html_content;
+    for(let nombre of nombres){
+        html += nombre;
+    }
+    response.send(html);
+});
+
+module.exports = router;
