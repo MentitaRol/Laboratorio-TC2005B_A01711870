f4935440ef9329ab75788596074ff590165a7c34
Luisa Fernanda Valdez Guillen <A01711870@tec.mx>
2025-03-01 16:25:19 -0600

    .

--- Laboratorio 11/routes/nombres.routes.js
+++ Laboratorio 11/routes/nombres.routes.js
@@ -5,11 +5,11 @@ const express = require('express');
 const router = express.Router();
 
 const html_content = `<!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lab 6: Programación orientada a eventos</title>
+    <title>Laboratorio 11: Express</title>
     <!-- Booststrap link -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
@@ -51,6 +51,7 @@ router.post('/agregar', (request, response, next) => {
     console.log(request.body);
     nombres.push(request.body.nombre);
     console.log(nombres);
+
     let html = html_content;
     for(let nombre of nombres){
         html += nombre;
