73213355a50eb80b421707b0cfff7647ddfd643d
Luisa Fernanda Valdez Guillen <A01711870@tec.mx>
2025-03-04 11:13:07 -0600

    log out

--- Laboratorio 11/routes/users.routes.js
+++ Laboratorio 11/routes/users.routes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const users_controller = require('../controllers/users.controller');
 router.get('/login', users_controller.get_login);
 router.post('/login', users_controller.post_login);
+router.get('/logout', users_controller.get_logout);
 
 module.exports = router;
